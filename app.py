1376b29de923baa08b802a184110aca90dd79d37
shaxriyordasturchi <shaxriyorcholiyev951@gmail.com>
2025-05-22 20:56:17 +0500

    app.py
    Signed-off-by: shaxriyordasturchi <shaxriyorcholiyev951@gmail.com>

--- app.py
+++ app.py
@@ -23,5 +23,20 @@ elif lab == "lab4":
 elif lab == "lab5":
     st.header("Laboratoriya 5 - Tizim monitoringi va tahlil")
     st.write("Bu yerda tizim monitoringi va tahlil bo‘yicha laboratoriya vazifalari.")
+elif lab == "lab6":
+    st.header("Laboratoriya 6 – Spektral Samaradorlik Analizi")
+    st.write("WDM tizimlarida kanal soni oshgani sari spektral samaradorlik qanday o'zgarishini ko'rsatadi.")
+elif lab == "lab7":
+    st.header("Laboratoriya 7 – OCDMA Kodingi va Interferensiya")
+    st.write("OCDMA kod uzunligi va foydalanuvchi soni asosida interferensiya tahlili.")
+elif lab == "lab8":
+    st.header("Laboratoriya 8 – PON Delay Tahlili")
+    st.write("PON tarmog‘ida masofa va foydalanuvchi soni asosida kechikish (delay) aniqlanadi.")
+elif lab == "lab9":
+    st.header("Laboratoriya 9 – Hybrid Tarmoqda Faol Foydalanuvchilar Yuki")
+    st.write("WDM-OCDMA-PON tarmoqlarida bir vaqtning o‘zida faol foydalanuvchilar soni bilan yuklanish tahlili.")
+elif lab == "lab10":
+    st.header("Laboratoriya 10 – BER tahlili barcha texnologiyalarda")
+    st.write("WDM, OCDMA, PON va ularning gibridlarida BER (Bit Error Rate) taqqoslanadi.")
 else:
     st.write("Noto‘g‘ri laboratoriya tanlandi.")
