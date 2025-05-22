0a560307e8864af1021f9e29c95dff5db671bd67
shaxriyordasturchi <shaxriyorcholiyev951@gmail.com>
2025-05-22 22:28:28 +0500

    app.py
    Signed-off-by: shaxriyordasturchi <shaxriyorcholiyev951@gmail.com>

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
-import login
+from login import app as login_app
 import lab1_wdm
 import lab2_ocdma
 import lab3_pon
+import lab4_hybrid
+import lab5_error_correction
 
 def main():
     if "logged_in" not in st.session_state:
@@ -11,10 +13,17 @@ def main():
         st.session_state.username = ""
 
     if not st.session_state.logged_in:
-        login.app()
+        login_app()
     else:
         st.sidebar.title(f"Xush kelibsiz, {st.session_state.username}!")
-        page = st.sidebar.selectbox("Sahifa tanlang", ["Bosh sahifa", "Admin", "Lab 1 - WDM", "Lab 2 - OCDMA", "Lab 3 - PON"])
+        page = st.sidebar.selectbox("Sahifa tanlang", [
+            "Bosh sahifa",  
+            "Lab 1 - WDM Kanal Simulyatsiyasi", 
+            "Lab 2 - OCDMA Kodlash va Dekodlash", 
+            "Lab 3 - PON Tarmog‘i Tuzilishi",
+            "Lab 4 - WDM/OCDMA Gibrid Arxitekturasi",
+            "Lab 5 - Tarmoqlarda Xatolikni Tahlil va Tuzatish"
+        ])
 
         if page == "Bosh sahifa":
             st.title("🏠 Bosh sahifa")
@@ -23,13 +32,21 @@ def main():
                 st.session_state.logged_in = False
                 st.session_state.username = ""
                 st.experimental_rerun()
-            admin.app()
-        elif page == "Lab 1 - WDM":
+
+        elif page == "Lab 1 - WDM Kanal Simulyatsiyasi":
             lab1_wdm.app()
-        elif page == "Lab 2 - OCDMA":
+
+        elif page == "Lab 2 - OCDMA Kodlash va Dekodlash":
             lab2_ocdma.app()
-        elif page == "Lab 3 - PON":
+
+        elif page == "Lab 3 - PON Tarmog‘i Tuzilishi":
             lab3_pon.app()
+
+        elif page == "Lab 4 - WDM/OCDMA Gibrid Arxitekturasi":
+            lab4_hybrid.app()
+
+        elif page == "Lab 5 - Tarmoqlarda Xatolikni Tahlil va Tuzatish":
+            lab5_error_correction.app()
 
 if __name__ == "__main__":
     main()
